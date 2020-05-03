df8df98db269ed4c942109e515c1ea97ef499caf
Kelly <kellyparks111@gmail.com>
2020-05-03 12:10:36 -0600

    SRP exercise and notes

---
+++ ArdalisRating/FilePolicySource.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+/**
+ * Instead of using low-level System.IO libraries throughout the code, just use it once in a simple class.
+ * This helps facilitate the "tell, don't ask" delegation of SOLID design.
+ */
+namespace ArdalisRating
+{
+    public class FilePolicySource
+    {
+        public string GetPolicyFromSource()
+        {
+            return File.ReadAllText("policy.json");
+        }
+    }
+}
